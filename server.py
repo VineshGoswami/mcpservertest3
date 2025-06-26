77577b42395a540085a3e84db0f8951feecd8301
VK Goswami <vineshgoswami45@gmail.com>
2025-06-26 19:58:11 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+mcp = FastMCP("server")
+@mcp.tool()
+def getinformation(topic:str)->str:
+    """
+    Get information about the topic,
+    Args:
+    topic: information of topic
+    """
+    return f"information about {topic}"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
+
